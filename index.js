295279b45351d7d6e56827cc40cf024d58c3f9ad
unknown <chirlychplin@gmail.com>
2021-09-01 15:13:48 -0400

    hola

--- index.js
+++ index.js
@@ -3,26 +3,50 @@ const $botton = document.querySelector('#add');
 let $content = document.querySelector('#content');
 let ul = document.querySelector('#ul');
 const $main = document.querySelector('#main');
-let completeTodo=[];
+let completeTodo = [];
 let todo=[];
 let form=null;
 let  value = 0;
 
 
+//complete
+let active = null;
+let all = null;
 
-$botton.addEventListener('click', () => {
+let complete = () =>{
+    const view = 
+    `
+    <div class="contaiener" id="contentComplete">
+        <button class="btnDeleted all" id="delete" onclick="empty()">delete all</button>
+    </div>
+    `;
+    return view;
+}
+let template = ()=>{
+    const view =
+    ` <div class="contaiener" id="content">
+      </div>
+    `;
+    return view;
+}
+
+
+
+/*$botton.addEventListener('click', () => {
     const node = document.createElement('label');
     const check = document.createElement('input');
     const span = document.createElement('span');
     check.className ='checkbox';
     span.innerHTML = $input.value;
     span.className = 'text';
-    span.setAttribute('value', value );
-    value= value+1;
+    node.setAttribute('value', value );
+    value = value + 1;
     node.appendChild(check);
     node.append(span);
     check.type="checkbox";
     $content.appendChild(node);
+
+    todo.push(node);
 
 
     for (let i = 0; i < $content.children.length; i++) {
@@ -30,30 +54,49 @@ $botton.addEventListener('click', () => {
     }  
    
 
-}) 
+}) */
+function add() {
+    debugger;
+    const node = document.createElement('label');
+    const check = document.createElement('input');
+    const span = document.createElement('span');
+    check.className ='checkbox';
+    span.innerHTML = $input.value;
+    span.className = 'text';
+    node.setAttribute('value', value );
+    value = value + 1;
+    node.appendChild(check);
+    node.append(span);
+    check.type="checkbox";
+    $content.appendChild(node);
+
+    todo.push(node);
+    console.log($content);
+    $main.appendChild($content);
+
+
+    for (let i = 0; i < $content.children.length; i++) {
+        $content.children[i].addEventListener('click', click);
+    }  
+}
 
 function click() { 
-    debugger;
-    this.children[1].classList.toggle('checkActive');
-    const valor = this.children[1].getAttribute('value');
-    const aux = ()=>{
-        for (let i = 0; i < completeTodo.length; i++) {
-            debugger;
-            if(completeTodo[i].getAttribute('value') === valor){
-                return false;
-                i=completeTodo.length;
-            }
-            
-        }
-        return true;
-    }
+    this.classList.toggle('checkActive');
+    const valor = this.getAttribute('value');
+   let aux =  completeTodo.some(element => element.getAttribute('value') === valor ); 
+   let indice;
+   debugger;
     if(aux){
-        completeTodo.push(this.children[1]);
+        indice = completeTodo.indexOf(this);
+        todo.push(this);
+        completeTodo[indice] = 0;
     }else{
-        completeTodo.pop(this.children[1]);
+        indice = todo.indexOf(this);
+        completeTodo.push(this);
+        todo[indice] = 0;
     }
-    
 }
+
 for(let i = 0; i < 3; i++){
     console.log(ul.children[i]);
     ul.children[i].addEventListener('click', router);
@@ -63,86 +106,86 @@ for(let i = 0; i < 3; i++){
 
 
 function router (){
-    debugger;
     const valor = this.value;
 
-    for (let i = 0; i < $content.children.length; i++) {
-        if($content.children[i].children.item(1).classList.contains("checkActive")){
-            completeTodo.push($content.children[i]);
-        }else{
-            todo.push($content.children[i]);
-        }
-    }  
     if(valor == '1'){
         remove(valor);
-        if(form != null){
-            form.style.display = 'block';
+        debugger;
+        if(form !== null){
+            reset(1); 
         }
-        for (let i = 0; i < completeTodo.length; i++) {
-            $content.appendChild(completeTodo[i]);
-            $content.appendChild(todo[i]);
+        
+        for (const element in completeTodo) {
+            if (completeTodo.length > 0) {
+                $content.appendChild(completeTodo[element]);
+            }
         }
-
-
+        for (const key in todo) {
+            if (todo.length > 0) {
+                $content.appendChild(todo[key]);
+            }
+        }
+        all=$content;
     }
     if(valor == '2'){
         remove(valor);
-        for (let i = 0; i < completeTodo.length; i++) {
-            $content.removeChild(completeTodo[i]);
-            $content.appendChild(todo[i]);
+        debugger;
+        if(form !== null){
+            reset(2);
         }
+        for (const key in completeTodo) {
+            if(todo.length>-1){
+                $content.removeChild(completeTodo[key]);
+            } 
+        }
+        for (const key in todo) {
+            if (todo.length > 0) {
+                $content.appendChild(todo[key]);
+            }
+        }
+        active=$content;
     }
     if(valor == '3'){
         remove(valor);
-        const deleted = document.createElement('button');
-        deleted.className ='btnDeleted';
-        deleted.setAttribute('id', 'delete');
-        deleted.innerText='delete all';
-        let first =$main.children[0];
-        for (let i = 0; i < $content.children.length; i++) {
-            $content.removeChild($content.children[i]);
+        form = document.querySelector('#formInput');
+        $main.innerHTML = complete();
+       
+        let $complete = document.querySelector('#contentComplete')
+        for (const key in completeTodo) {
+            $complete.appendChild(completeTodo[key]);  
         }
-        for (let i = 0; i < completeTodo.length; i++) {
-            $content.appendChild(completeTodo[i]);
-            
-        }
-    } 
+    }
+    debugger;
+
 }
+function reset(value) {
+    $main.firstChild.remove();
+    debugger;
+    $main.innerHTML = template();
+    $main.insertBefore(form, $main.firstChild);
+    if(value == 2){
+       for (const key in todo.length) {
+            $content.appendChild(todo[key]);
+       }
+    }else{
+    
+    }
+    $main.insertBefore(form, $main.firstChild);
+}
+
 function remove(value) {
     for(let i = 0; i < 3; i++){
-        debugger
+        
         if(ul.children[i].value == value){
-            
            ul.children[i].classList.add("liActive");
         }else{
-            ul.children[i].classList.remove("liActive"); 
+           ul.children[i].classList.remove("liActive"); 
         }
     }
 }
-
-
-
-
-
-
-
-
-
-
-/*
-for(let i = 0; i < $content.children.length; i++){
-    if($content.children[i].className === 'checkbox'){
-        debugger
-
-        $content.children[i].addEventListener('click', ()=>{
-            for (let i = 0; i < $content.children.length; i++) {
-                if($content.children[i].className === 'span'){
-                    $content.children[i].className = 'checkActive';               
-                }
-            }
-        });
-
-    }
-    
-}
-*/
+function empty() {
+    //empty your array
+    completeTodo = [];
+    $main.innerHTML = complete();
+    //$main.innerHTML = complete();
+};
